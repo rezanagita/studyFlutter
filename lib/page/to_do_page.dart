18953968894a411a190a1c5224dae5bb3baaa806
REZA NAGITA <rezagitta@com>
2024-05-28 19:11:39 +0700

    mengkases web service/API

---
+++ lib/page/to_do_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_webservice/model/to_do.dart';
+
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({Key? key}) : super(key: key);
+
+  @override
+  _ToDoPageState createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+  Future<List<ToDo>> fetchToDo() async {
+    var url = Uri.parse(
+        'https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object ToDo
+    List<ToDo> listToDo = [];
+    for (var d in data) {
+      if (d != null) {
+        listToDo.add(ToDo.fromJson(d));
+      }
+    }
+
+    return listToDo;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
